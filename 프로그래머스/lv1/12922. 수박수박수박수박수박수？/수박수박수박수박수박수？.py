9688bca8267d75de8bbee0aace44f062d68a69d7
casa-python <135565166+casa-python@users.noreply.github.com>
2023-06-09 17:27:03 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -0,0 +1,12 @@
+def solution(n):
+    
+    return '수박'*(n//2) + '수'*(n%2)
+ 
+#----------------------------------------O
+# def solution(n):
+#     answer = ''
+    
+#     a = "수박" * 5000
+#     answer = a[:n]
+    
+#     return answer
