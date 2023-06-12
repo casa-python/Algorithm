917ab8820fb36c136882f861b4c17916b473ea26
casa-python <135565166+casa-python@users.noreply.github.com>
2023-06-12 09:37:26 +0900

    Update 같은 숫자는 싫어.py

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,9 +1,12 @@
 def solution(arr):
-    answer = []
-    answer.append(arr[0])
+    answer = []                     # 결과를 저장할 빈 리스트 생성
+    answer.append(arr[0])           # 첫 번째 요소를 결과 리스트에 추가
+
+    # 두 번째 요소부터 마지막 요소까지 반복문을 수행
     for i in range(1, len(arr)):
-        if arr[i] == arr[i - 1]:    # 연속된 값이면 통과(제거)
-            continue
-        else:                       # 앞과 다른 값이면 추가
-            answer.append(arr[i])
-    return answer
+        if arr[i] == arr[i - 1]:    # 현재 요소와 이전 요소가 같은지 비교
+            continue                # 같으면 건너뛰고 다음 요소로 진행
+        else:
+            answer.append(arr[i])   # 이전 요소와 다른 경우 결과 리스트에 추가
+    
+    return answer                   # 결과 리스트 반환
