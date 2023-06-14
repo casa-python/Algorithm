fc4080c2aee7df2e3e8e1fc168954bdab5f947a3
casa-python <135565166+casa-python@users.noreply.github.com>
2023-06-14 09:37:36 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.py
@@ -0,0 +1,16 @@
+#--------------------------------------------------O
+def solution(num):
+    
+    return ("Even" if not num%2 else "Odd")
+# 정수 0, 1이 False와 True를 나타내는 것을 이용한 함수
+
+#--------------------------------------------------O
+# def solution(num):
+    
+#     if num % 2 == 0:       # 짝수면 Even 출력
+#         answer = "Even"
+        
+#     else:                  # 홀수면 Odd 출력
+#         answer = "Odd"
+    
+#     return answer
